44161a91e18dc38120123c7d163a89a356ad8a9b
Wim von Benecke <boneswvb@gmail.com>
2020-02-24 03:26:21 +0200

    project to beused for exersizing custom hooks.

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react"
+import './App.css';
+/**
+ * Pop Quiz!
+ * 
+ * Refactor the class component below to use hooks instead
+ */
+
+
+class App extends Component {   
+    state = {
+        count: 0
+    }
+    
+    increment = () => {
+        this.setState(prevState => ({count: prevState.count + 1}))
+    }
+    
+    render() { 
+        return (
+            <div>
+                <h1>The count is {this.state.count}</h1>
+                <button onClick={this.increment}>Add 1</button>
+            </div>
+        )
+    }
+}
+
+export default App
